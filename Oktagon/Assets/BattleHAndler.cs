a855425ec9e570ec97c0a4525b41510db4766387
Nouiri-Hafsa <43516162+N-HAFSA@users.noreply.github.com>
2019-09-30 01:07:17 +0200

    last

---
+++ Oktagon/Assets/BattleHAndler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleHAndler : MonoBehaviour
+{
+
+    //charachtere in battle
+    [SerializeField] private Transform pfCharBattele;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(pfCharBattele, new Vector3(-10, 0), Quaternion.identity);
+        Instantiate(pfCharBattele, new Vector3(+10, 0), Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnCharacter(bool isPlayerTeam)
+    {
+        Vector3 position;
+        if (isPlayerTeam)
+        {
+            position = new Vector3(-10, 0);
+        }
+        else
+        {
+            position = new Vector3(+10, 0);
+        }
+        Instantiate(pfCharBattele, position, Quaternion.identity);
+    }
+}
---
+++ Oktagon/Assets/Scripts/weapon.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class weapon : MonoBehaviour
+{
+   // public Rigidbody2D rb;
+   // public Rigidbody2D hook;
+
+  //  public float releaseTime = .15f;
+   // public float maxDragDistance = 2f;
+
+   //the start angle
+    //the weapon==>arrow/red ball/blue ball
+    ///public GameObject nextBall;
+
+    //private bool isPressed = false;
+
+
+    public float offset;
+
+    public GameObject projectile;
+   // public GameObject shotEffect;
+    public Transform shotPoint;
+    //public Animator camAnim;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    private void Update()
+    {
+        // Handles the weapon rotation
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        if (timeBtwShots <= 0)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+               // Instantiate(shotEffect, shotPoint.position, Quaternion.identity);
+                //camAnim.SetTrigger("shake");
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+
+    }
+    /*  if (isPressed)
+      {
+          Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+          if (Vector3.Distance(mousePos, hook.position) > maxDragDistance)
+              rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
+          else
+              rb.position = mousePos;
+      }*/
+//}
+
+    /*void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+
+        if (nextBall != null)
+        {
+            nextBall.SetActive(true);
+        }
+        else
+        {
+            enemy.EnemiesAlive = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+    }*/
+
+}
